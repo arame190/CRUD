653009d10f8ba4218c6c11516919522e9f96c2ec
arame190 <arame.sukiasyan.y@tumo.org>
2024-02-03 15:20:26 +0400

    finish

--- server.js
+++ server.js
@@ -2,36 +2,36 @@ var express = require("express");
 var path = require("path");
 const bodyParser = require('body-parser');
 var app = express();
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const { ObjectId } = require('mongoose').Types;
 
 const mongoose = require('mongoose');
 const connectionString = 'mongodb+srv://arame_190:aralar666@cluster0.jompnul.mongodb.net/Tumo_product';
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
 app.use(express.static('public'));
+
 app.get("/", function (req, res) {
     mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'Connection error:'));
     db.once('open', async () => {
-        console.log('Connected to MongoDB!');
-
         try {
-            const result = await mongoose.connection.db.collection('products').find().toArray();
+            let result = await mongoose.connection.db.collection('products').find().toArray()
             res.render('../public/form.ejs', {
-                info: result
-            })
-
+                obj: result
+            });
         } catch (error) {
             console.error('Error retrieving movies:', error);
         } finally {
             mongoose.connection.close();
-        };
-    });
+        }
+    })
 });
 
-app.post('/addName', (req, res) => {
+app.post('/addName', async (req, res) => {
     const name = req.body.name;
     const surname = req.body.surname;
     const age = req.body.age;
@@ -43,88 +43,94 @@ app.post('/addName', (req, res) => {
     db.on('error', console.error.bind(console, 'Connection error:'));
     db.once('open', async () => {
         console.log('Connected to MongoDB!');
-
         try {
             let result = await mongoose.connection.db.collection('products').insertOne({
-                 name: name,surname: surname, age: age, password: password,phonenumber: phonenumber, email: email}
-            );
-                res.redirect('/')
+                name: name,
+                surname:surname,
+                age: age,
+                password: password,
+                phonenumber: phonenumber,
+                email: email,
+            })
+            res.redirect('/')
         } catch (error) {
             console.error('Error retrieving movies:', error);
         } finally {
             mongoose.connection.close();
-        };
-    });
+        }
+    })
 });
 
 app.get("/delete/:id", function (req, res) {
+ var id = req.params.id;
+    mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'Connection error:'));
+    db.once('open', async () => {
+        try {
+            await mongoose.connection.db.collection('products').deleteOne({_id: new ObjectId(id)});
+            res.redirect('/')
+        } catch (error) {
+            console.error('Error retrieving movies:', error);
+        } finally {
+            mongoose.connection.close();
+        }
+    })
+});
+
+app.get("/update/:id", function (req, res) {
     var id = req.params.id;
-       mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
-       const db = mongoose.connection;
-       db.on('error', console.error.bind(console, 'Connection error:'));
-       db.once('open', async () => {
-           try {
-               let result = await mongoose.connection.db.collection('products').deleteOne({_id: new ObjectId(id)});
-               res.json(result);
-           } catch (error) {
-               console.error('Error retrieving movies:', error);
-           } finally {
-               mongoose.connection.close();
-           }
-       })
-   });
-
-   app.get("/update/:id", function (req, res) {
-       var id = req.params.id;
-       mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
-       const db = mongoose.connection;
-       db.on('error', console.error.bind(console, 'Connection error:'));
-       db.once('open', async () => {
-           try {
-               let result = await mongoose.connection.db.collection('products').findOne({_id: new ObjectId(id)});
-               res.render('../public/update.ejs', {
-                   obj: result
-               });
-           } catch (error) {
-               console.error('Error retrieving movies:', error);
-           } finally {
-               mongoose.connection.close();
-           }
-       })
-   });
+    mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'Connection error:'));
+    db.once('open', async () => {
+        try {
+            let result = await mongoose.connection.db.collection('products').findOne({_id: new ObjectId(id)});
+            res.render('../public/update.ejs', {
+                obj: result
+            });
+        } catch (error) {
+            console.error('Error retrieving movies:', error);
+        } finally {
+            mongoose.connection.close();
+        }
+    })
+});
 
 
-   app.post("/updateData", function (req, res) {
-       const name = req.body.name;
-       const surname = req.body.surname;
-       const age = req.body.age;
-       const password = req.body.password;
-       const phonenumber = req.body.phonenumber;
-       const email = req.body.email;
-       const id = req.body.id;
+app.post("/updateData", function (req, res) {
+    const name = req.body.name;
+    const surname = req.body.surname;
+    const age = req.body.age;
+    const password = req.body.password;
+    const phonenumber = req.body.phonenumber;
+    const email = req.body.email;
+    const id = req.body.id;
 
-       mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
-       const db = mongoose.connection;
+    mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+    const db = mongoose.connection;
 
-       db.on('error', console.error.bind(console, 'Connection error:'));
+    db.on('error', console.error.bind(console, 'Connection error:'));
 
-       db.once('open', async () => {
-           console.log('Connected to MongoDB!');
+    db.once('open', async () => {
+        console.log('Connected to MongoDB!');
 
-           try {
-               let result = await mongoose.connection.db.collection('products').updateOne(
-                   { _id: new ObjectId(id) },
-                   { $set: { name: name, surname: surname, age: age, password: password, phonenumber: phonenumber, email: email, } }
-               );
+        try {
+            let result = await mongoose.connection.db.collection('products').updateOne(
+                { _id: new ObjectId(id) },
+                { $set: { name: name, surname: surname, age: age, password: password, phonenumber: phonenumber, email: email} }
+            );
+            res.redirect('/')
 
-               res.json(result);
-           } catch (error) {
-               console.error('Error updating product:', error);
-           } finally {
-               mongoose.connection.close();
-           }
-       });
-   });
+        } catch (error) {
+            console.error('Error updating product:', error);
+        } finally {
+            mongoose.connection.close();
+        }
+    });
+
+});
+
 
 
 app.listen(3000, function () {
